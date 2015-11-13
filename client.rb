f323b7b575b1a668de9fbe5a1b57a90303058646
root <root@chef-test.spark.test>
2015-11-13 09:48:19 +0900

    first-commit

---
+++ client.rb
@@ -0,0 +1,4 @@
+log_level        :info
+log_location     STDOUT
+chef_server_url  'https://chef-test.spark.test:443'
+validation_client_name 'spark-test-validator'
---
+++ cookbooks/chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: chef
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
